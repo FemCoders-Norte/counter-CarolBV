831534e972c789fca4b1e1a19540aafb42204ad8
CarolBV <carolina.botella.valero@gmail.com>
2024-07-16 17:21:44 +0200

    counter done

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,13 +1,21 @@
 function add(){
     //Escribir tu código que aumenta
+    const numberElement = document.getElementById('number');
+    let number = parseInt(numberElement.textContent, 10);
+    numberElement.textContent = number + 1;
 }
 
 function reset(){
     //Escribir tu código que hace un reset a 0
+    const numberElement = document.getElementById('number');
+    numberElement.textContent = 0;
 }
 
 function decrease(){
     //Escribir tu código que resta
-};
+    const numberElement = document.getElementById('number');
+    parseInt(numberElement.textContent,10);
+    numberElement.textContent = 0;
+}
 
 export {add, reset, decrease}
